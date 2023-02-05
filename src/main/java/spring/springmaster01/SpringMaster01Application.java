fcbb4da879efa457217ee5f29b5af118133beb1c
prolkh <prolkh@gmail.com>
2023-02-05 23:51:55 +0900

    스프링 입문 - 코드로 배우는 스프링 부트, 웹 MVC, DB 접근 기술 프로젝트 생성

---
+++ src/main/java/spring/springmaster01/SpringMaster01Application.java
@@ -0,0 +1,13 @@
+package spring.springmaster01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMaster01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMaster01Application.class, args);
+	}
+
+}
---
+++ src/test/java/spring/springmaster01/SpringMaster01ApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.springmaster01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMaster01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
